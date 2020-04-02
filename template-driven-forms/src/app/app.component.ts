37d2058d0173a34b55a9633f673aca16c70a437f
cristianmarquezp <cristian.marquez@uptc.edu.co>
2020-04-02 11:00:06 -0500

    Formulario sin ngModel

--- template-driven-forms/src/app/app.component.ts
+++ template-driven-forms/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Stock } from './model/stock';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'template-driven-forms';
+  public stock: Stock;
+
+  constructor(){
+    this.stock = new Stock('test','',0,0);
+  }
 }
