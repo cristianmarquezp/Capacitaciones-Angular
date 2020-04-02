0a6d5fccacd5f4cd583648e150125fdf89b0e53b
cristianmarquezp <cristian.marquez@uptc.edu.co>
2020-04-02 15:47:21 -0500

    Formulario con control validity

--- template-driven-forms/src/app/stock/create-stock/create-stock.component.ts
+++ template-driven-forms/src/app/stock/create-stock/create-stock.component.ts
@@ -10,18 +10,24 @@ export class CreateStockComponent implements OnInit {
 
   public stock : Stock;
   public confirmed = false;
+  public exchanges = ['NYSE', 'NASDAQ', 'OTHER']
 
 
   constructor() { 
-    this.stock  = new Stock('test','', 0,0,'NASDAQ');
+    this.stock  = new Stock('','', 0,0,'NASDAQ');
   }
 
   setStockPrice(price){
     this.stock.price = price;
     this.stock.previousPrice = price;
   }
-  createStock() {
-    console.log('Creating stock ', this.stock);
+  createStock(stockForm) {
+    console.log('Stock form', stockForm);
+    if (stockForm.valid) {
+      console.log('Creating stock ', this.stock);
+    } else {
+      console.error('Stock form is in an invalid state');
+    }
   }
 
   ngOnInit() {
